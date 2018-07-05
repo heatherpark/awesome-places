f30cac61c7ca060ed699ae17ef44fc9f8edf94e0
Heather Park <hthr.prk@gmail.com>
2018-07-04 19:22:04 -0700

    Pass delete item event handler

--- src/components/PlaceList/PlaceList.tsx
+++ src/components/PlaceList/PlaceList.tsx
@@ -4,6 +4,7 @@ import { View, Text } from 'react-native';
 import ListItem from '../ListItem/ListItem';
 
 interface IProps<T> {
+  onItemDeleted: (index: number) => void;
   places: T[];
 }
 
@@ -11,6 +12,7 @@ const placeList: React.SFC<IProps<string>> = (props: IProps<string>) => (
   <View>
     {props.places.map((list: string, index: number) =>
       <ListItem
+        onItemPressed={() => props.onItemDeleted(index)}
         key={index}
         place={list} />)}
   </View>
