521f74f20f36e7e5c6d832ccc4f52c603621dc72
Heather Park <hthr.prk@gmail.com>
2018-07-05 10:14:33 -0700

    Have place object hold image property

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, View } from 'react-native';
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
 import { Place } from './src/constants';
+import placeImage from './src/assets/tokyo-main.jpg';
 
 interface AppState {
   places: Place[];
@@ -18,7 +19,8 @@ export default class App extends React.Component<{}, AppState> {
     const newPlace = {
       // TODO: Use id generator
       key: Math.random(),
-      value: placeName
+      name: placeName,
+      image: placeImage
     };
 
     this.setState((prevState: AppState) => ({
