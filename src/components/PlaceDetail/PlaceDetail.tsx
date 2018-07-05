425b9dd9728556e8e52bac7792aac79a8e165f75
Heather Park <hthr.prk@gmail.com>
2018-07-05 12:01:55 -0700

    Create PlaceDetail component

---
+++ src/components/PlaceDetail/PlaceDetail.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Button, Image, Modal, View, Text } from 'react-native';
+
+interface PlaceDetailProps {
+  selectedPlace: {
+    placeName: string;
+    placeImage: {};
+  }
+}
+
+const placeDetail: React.SFC<PlaceDetailProps> = props => (
+  <Modal>
+    <View>
+      <Image source={props.selectedPlace.placeImage} />
+      <Text>{props.selectedPlace.placeName}</Text>
+      <View>
+        <Button />
+        <Button />
+      </View>
+    </View>
+  </Modal>
+);
+
+export default placeDetail;
