a7ea918f5bc64ebd3dab97180612ed60e4a282f1
Heather Park <hthr.prk@gmail.com>
2018-07-05 12:46:55 -0700

    Set up modal

--- src/components/PlaceDetail/PlaceDetail.tsx
+++ src/components/PlaceDetail/PlaceDetail.tsx
@@ -1,24 +1,37 @@
 import * as React from 'react';
-import { Button, Image, Modal, View, Text } from 'react-native';
+import { Button, Image, Modal, StyleSheet, View, Text } from 'react-native';
+
+import { Place } from '../../constants';
 
 interface PlaceDetailProps {
-  selectedPlace: {
-    placeName: string;
-    placeImage: {};
-  }
+  selectedPlace: Place | null;
 }
 
-const placeDetail: React.SFC<PlaceDetailProps> = props => (
-  <Modal>
-    <View>
-      <Image source={props.selectedPlace && props.selectedPlace.placeImage} />
-      <Text>{props.selectedPlace && props.selectedPlace.placeName}</Text>
-      <View>
-        <Button />
-        <Button />
+const placeDetail: React.SFC<PlaceDetailProps> = props => {
+  const renderModal = (selectedPlace: Place) => (
+    <React.Fragment>
+      <Image source={selectedPlace.image} />
+      <Text>{selectedPlace.name}</Text>
+    </React.Fragment>
+  );
+
+  return (
+    <Modal visible={props.selectedPlace !== null}>
+      <View style={styles.modalContainer}>
+        {props.selectedPlace && renderModal(props.selectedPlace)}
+        <View>
+          <Button onPress={() => { }} title="Delete" color="red" />
+          <Button onPress={() => { }} title="Close" />
+        </View>
       </View>
-    </View>
-  </Modal>
-);
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    margin: 22
+  }
+});
 
 export default placeDetail;
