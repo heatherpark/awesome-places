1f6d7f95f6dd510cc2483b0bc56ede0869a3a979
Heather Park <hthr.prk@gmail.com>
2018-07-10 11:53:30 -0700

    Add initial state to places reducer file

---
+++ src/store/reducers/places.ts
@@ -0,0 +1,12 @@
+import { PlacesState } from '../../constants';
+
+const initialState: PlacesState = {
+  places: [],
+  selectedPlace: null
+};
+
+const reducer = (state: PlacesState = initialState, action) => {
+
+};
+
+export default reducer;
