816a841660d7eeb5a62d7996be69cd1c2d820888
Heather Park <hthr.prk@gmail.com>
2018-07-10 17:04:54 -0700

    Add place action creators

---
+++ src/store/actions/places.ts
@@ -0,0 +1,21 @@
+import * as actionTypes from './action-types';
+
+export const addPlace = placeName => {
+  return {
+    type: actionTypes.ADD_PLACE,
+    placeName
+  };
+};
+
+export const deletePlace = () => {
+  return {
+    type: actionTypes.DELETE_PLACE
+  };
+};
+
+export const selectPlace = key => {
+  return {
+    type: actionTypes.SELECT_PLACE,
+    placeKey: key
+  };
+};
